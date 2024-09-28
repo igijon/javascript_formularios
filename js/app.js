74d4c62468319ff5da5a689bcb759b17b3e6cf60
Inma Gijón <igijoninf@gmail.com>
2024-09-28 14:30:23 +0200

    Ciclo de formulario tradicional con JS

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
-const btnEnviar = document.getElementById('btnEnviar')
+const form = document.getElementById('formulario')
 
-btnEnviar.addEventListener('click', () => {
-    // Obtener los valores de los campos
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let genero = document.querySelector('input[name="genero"]:checked'); // Radio button seleccionado
-    let pais = document.getElementById("pais").value;
-
-    // Validar que los campos no estén vacíos
-    if (nombre === "" || email === "" || password === "") {
-        alert("Por favor, rellena todos los campos.");
-    } else if (!genero) {
-        alert("Por favor, selecciona un género.");
-    } else if (pais === "") {
-        alert("Por favor, selecciona un país.");
-    } else {
-        alert("Formulario enviado correctamente.");
-        document.getElementById("formulario").reset(); // Limpia el formulario
-    }
+form.addEventListener('submit', (e) => {
+    e.preventDefault() //Evitamos el envío del formulario para este ejemplo
+    console.log('Entra')
+    alert('Formulario enviado correctamente')
 })
