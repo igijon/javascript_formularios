90ed38393173e432ca16dc18cd7fd74276a3bc07
Inma Gijón <igijoninf@gmail.com>
2024-09-28 14:57:43 +0200

    Formulario con validación JS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const form = document.getElementById('formulario')
 
+const validar = (e) => {
+    console.log('entra')
+    let passwd = document.getElementById('password').value
+    let passRgex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    // Al menos una letra mayúscula, una minúscula, un dígito y un carácter especial y al menos 8 caracteres
+    let result = passRgex.test(passwd)
+    console.log(result)
+    return result
+}
+
 form.addEventListener('submit', (e) => {
-    e.preventDefault() //Evitamos el envío del formulario para este ejemplo
-    console.log('Entra')
-    alert('Formulario enviado correctamente')
+    if(!validar()) {
+        e.preventDefault()
+        alert("Error, la contraseña no cumple el patrón")
+    }
 })
